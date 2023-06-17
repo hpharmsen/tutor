1b9b5529e47e88ffcc74bdd6b5ed5dd3d3e7149a
HP Harmsen <hph@oberon.nl>
2023-06-18 00:57:47 +0200

    Added settings.py to manage tutor settings.

--- tutor.py
+++ tutor.py
@@ -3,7 +3,7 @@ import sys
 
 from gpteasy import GPT, Repl, CommandHandler
 import gpteasy.display as gpt_display
-import gpteasy.settings as gpt_setings
+import settings
 from synthesize import say
 
 STATUS_NEXT_QUESTION = 1
@@ -13,7 +13,7 @@ STATUS_ANSWER = 2
 class Tutor(GPT):
     def __init__(self):
         super().__init__()
-        self.system = gpt_setings.system_message
+        self.system = settings.system_message
         self.hard_concepts = []
         self.message_memory = 4
         self.last_question = ''
@@ -31,14 +31,12 @@ class Tutor(GPT):
                 Generate a new sentence that includes one or more of the concepts I got wrong"""
             else:
                 prompt = "Generate a new sentence"
-            prompt += f"\ninclude the word {gpt_setings.random_word()}"
-            if gpt_setings.get_settings()['debug'] == '1':
-                gpt_display.color_print(prompt, color=gpt_display.DEBUG_COLOR2)
+            prompt += f"\ninclude the word {settings.random_word()}"
         else:
             # Ask the user for a prompt
             prompt = ''
             while not prompt:
-                prompt = input(f"{gpt_setings.get_settings()['language']}: ")
+                prompt = input(f"{settings.get_settings()['language']}: ")
         return prompt
 
     def chat(self, prompt, add_to_messages=True):
@@ -46,9 +44,6 @@ class Tutor(GPT):
         # Check if there's a concept that went wrong last time. If so, include it in the prompt.
 
         message = super().chat(prompt, add_to_messages=add_to_messages)
-
-        if gpt_setings.get_settings()['debug'] == '1':
-            gpt_display.color_print(message.text, color=gpt_display.DEBUG_COLOR1)
 
         reply = message.content()
         match reply['type']:
@@ -63,43 +58,39 @@ class Tutor(GPT):
                     hard_concept = {'question': self.last_question, 'answer': prompt, 'analysis': reply['response']}
                     self.hard_concepts.append(hard_concept)
                 self.status = STATUS_NEXT_QUESTION
+                self.messages = [] # Clear the message history. Old sentences only increase token count
         message.text = reply['response']
         return message
 
     def after_response(self, message):
         reply = message.content()
-        if reply['type'] == 'analysis' and gpt_setings.get_settings().get('play_audio') == '1':
+        if reply['type'] == 'analysis' and settings.get_settings().get('play_audio') == '1':
             sentence = self.last_answer if reply['verdict'] == 'right' else reply['right_answer']
-            say(sentence, language=gpt_setings.get_settings()['language'])
+            say(sentence, language=settings.get_settings()['language'])
 
 
 def handle_level(level: str):
     level = level.upper()
-    accepted_levels = gpt_setings.WORDS_PER_LEVEL.keys()
+    accepted_levels = settings.WORDS_PER_LEVEL.keys()
     if level not in accepted_levels:
         gpt_display.color_print(f"Error: level must be one of {', '.join(accepted_levels)}",
                                 color=gpt_display.SYSTEM_COLOR)
     else:
-        gpt_setings.get_settings()['level'] = level
+        settings.get_settings()['level'] = level
         gpt_display.color_print(f'language level set to {level}', color=gpt_display.SYSTEM_COLOR)
     return True
 
 
-def handle_debug(debug: str):
-    gpt_setings.get_settings()['debug'] = debug
-    gpt_display.color_print(f'debug set to {debug}', color=gpt_display.SYSTEM_COLOR)
-    return True
-
-
 if __name__ == "__main__":
-    s = gpt_setings.get_settings()
+    s = settings.get_settings()
     gpt_display.color_print(f"Hello, I am your {s['language']} tutor on {s['level']} level. " +
                             f"I will help you learn {s['language']}.\n" +
-                            f"I will give you sentences in English ' +"
+                            f"I will give you sentences in English " +
                             f"and you will have to translate them into {s['language']}.\n" +
                             f"Here's your first sentence:\n", color=gpt_display.SYSTEM_COLOR)
     gpt = Tutor()
     gpt.model = s['model']
+    gpt.debug = True
 
     # Load session if passed as a command line argument
     if len(sys.argv) > 1:
@@ -108,7 +99,7 @@ if __name__ == "__main__":
     # Add a command handler that handles special commands like model parameters and system settings
     command_handler = CommandHandler(gpt)
     command_handler.add_command('level', handle_level, ":level - Set the language level (A1..C2)")
-    command_handler.add_command('debug', handle_debug, ":debug - set to 1 displays all prompts and model replies")
+
 
     # Start the interactive prompt
     repl = Repl(gpt, command_handler.handle_command)
